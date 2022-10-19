79debf2d647907b07216553f3179a76eff173011
Peter Muessig <peter.muessig@sap.com>
2022-10-19 14:57:44 +0200

    Some more tweaks

--- bookshop/app/perf-webapp/webapp/Component.js
+++ bookshop/app/perf-webapp/webapp/Component.js
@@ -1,6 +1,8 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/ComponentSupport",
+    "sap/ui/core/date/Gregorian"
 ], function (UIComponent, JSONModel) {
     "use strict"
     return UIComponent.extend(
